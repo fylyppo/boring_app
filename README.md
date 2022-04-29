fc49deef22adf0fcf203fc3dd7612358f86905f8
fylyppo <63547653+fylyppo@users.noreply.github.com>
2022-04-29 19:04:32 +0200

    Add files and set up Firebase

---
+++ README.md
@@ -0,0 +1,3 @@
+# boring_app
+
+A new flutter project to train coding skills.
---
+++ android/app/src/main/kotlin/com/example/boring_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.filiptuzimek.boring_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
