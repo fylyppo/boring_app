f20210411b6034d52e2eba8b60f511225fe9e836
fylyppo <63547653+fylyppo@users.noreply.github.com>
2022-05-01 10:27:47 +0200

    Auth facade interface

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,15 @@
+import 'package:boring_app/domain/auth/value_objects.dart';
+import 'package:dartz/dartz.dart';
+import 'auth_failure.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
